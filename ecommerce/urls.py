6044e47475795628f77086c24de8485e72584600
natnaelian <natnaelian@gmail.com>
2025-10-28 11:56:29 +0300

    fixed urls

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK", content_type="text/plain")
 
 urlpatterns = [
+    path("", health_check, name="health"),
     path("admin/", admin.site.urls),
     path("api/users/", include("users.urls")),
     path("api/products/", include("products.urls")),
